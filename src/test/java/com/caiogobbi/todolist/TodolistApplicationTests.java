06eefd8534cc30cff066de06082c46d891676768
caiogobbi <caio.cameyo@gmail.com>
2023-10-12 16:08:19 -0300

    Initial Commit

---
+++ src/test/java/com/caiogobbi/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiogobbi.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
