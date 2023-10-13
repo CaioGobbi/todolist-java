cd78102e10418dee5237ee5fc8bcbb508d09aa18
caiogobbi <caio.cameyo@gmail.com>
2023-10-13 09:44:54 -0300

    UserController & UserModel

---
+++ src/main/java/com/caiogobbi/todolist/user/UserModel.java
@@ -0,0 +1,11 @@
+package com.caiogobbi.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    private String Username;
+    private String name;
+    private String password;
+}
